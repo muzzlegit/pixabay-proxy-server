60c849ff3e9a68c5abc53f003598bf111de400a3
Roman Vozniuk <muzzlestore@gmail.com>
2021-11-03 00:51:59 +0200

    2

--- fetch-image.js
+++ fetch-image.js
@@ -5,8 +5,8 @@ const endpoint = 'https://pixabay.com/api';
 const fetchImage = ( query, page, perPage ) => {
 
   const apikey = process.env.API_KEY;
-  const url = `${endpoint}/?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=${perPage}&key=${apikey}`;
-  
+  // const url = `${endpoint}/?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=${perPage}&key=${apikey}`;
+  const url = "https://pixabay.com/api?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=24154374-c1e84db869e410d68acac2009"
   return fetch(url).then(res => res.json());
 }
 module.exports = fetchImage;
