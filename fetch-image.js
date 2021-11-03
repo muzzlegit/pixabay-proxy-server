c791f4e5826c45e11fef4ed0de35ee88cd847135
muzzlegit <muzzlestore@gmail.com>
2021-11-03 11:03:02 +0200

    fix

--- fetch-image.js
+++ fetch-image.js
@@ -2,10 +2,10 @@ const fetch = require('node-fetch');
 
 const endpoint = 'https://pixabay.com/api';
 
-const fetchImage = ( {  searchQuery, page, perPage  } ) => {
+const fetchImage = ( { query } ) => {
 
   const apikey = process.env.API_KEY;
-  const url = `${endpoint}/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=${perPage}&key=${apikey}`;
+  const url = `${endpoint}/?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=${perPage}&key=${apikey}`;
   // const url = "https://pixabay.com/api?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=24154374-c1e84db869e410d68acac2009"
   return fetch(url).then(res => res.json()).then(console.log);
 }
--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const PORT = process.env.PORT || 8888;
 app.use(cors('*'));
 
 app.get('/img', (req, res) => {
-  const { searchQuery, page, perPage } = req.query;
-
-  fetchImage(searchQuery, page, perPage)
+  const { query } = req.query;
+  console.log(req.query);
+  fetchImage( query )
     .then(image => res.status(200).json(image))
     .catch(error => res.status(400).json({ message: error }));
- });
+});
 app.listen(PORT, () => {
   console.log(`Application server is running on port ${PORT}`);
 });
