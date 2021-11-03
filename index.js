c1dc3eba8dbabd4d3f2a9c538b9bc1ffba0bcb2a
muzzlegit <muzzlestore@gmail.com>
2021-11-03 12:08:52 +0200

    fix

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ const PORT = process.env.PORT || 8888;
 app.use(cors('*'));
 
 app.get('/img', (req, res) => {
-  const { query } = req.query;
-  console.log(req.query);
+  // const { query } = req.query;
+
   fetchImage( query )
     .then(image => res.status(200).json(image))
+    // .then(res => console.log(res))
     .catch(error => res.status(400).json({ message: error }));
 });
 app.listen(PORT, () => {
